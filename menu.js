2954ee90ecdc2c7650554295a9a23decb0de7220
oscar100k <119451820+oscar100k@users.noreply.github.com>
2022-11-30 15:30:50 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,18 @@
+const menuInicial = document.querySelector('.menu'); 
+const menuFinal = document.querySelector('.menu-navegacion');
+
+console.log(menuInicial);
+console.log(menuFinal);
+
+
+menuInicial.addEventListener('click', ()=>{
+menuFinal.classList.toggle("spread")
+})
+
+window.addEventListener('click', e=>{
+    if(menuFinal.classList.contains('spread') && e.target != menuFinal && e.target != menuInicial){
+
+
+        menuFinal.classList.toggle("spread")
+    }
+})
